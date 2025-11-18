9395a3c3db5df51a00ea84bf7bd780add89d1cef
srecko <vasilije.sretenovic0503@gmail.com>
2025-11-18 11:26:06 +0100

    Promenjen link za google mapu sa registrovanim biz

--- index.php
+++ index.php
@@ -429,7 +429,7 @@ realizacije.</p>
 
             <div class="map-embed" role="region" aria-label="Mapa lokacije">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d283.31245850456173!2d20.535917557215527!3d44.787767638535065!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ssr!2srs!4v1758751393844!5m2!1ssr!2srs"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2831.655235600501!2d20.535947699999998!3d44.7878336!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475a71875595ec49%3A0x9bed6aa5361aefcb!2sAdatel%20doo%20Beograd!5e0!3m2!1sen!2srs!4v1763461335124!5m2!1sen!2srs" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
                 allowfullscreen
                 loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"></iframe>
